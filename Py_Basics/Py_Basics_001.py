dc63f41b8797f575e2a2c7556a714df84db3ba6c
desubhajit <subhajit.de@yahoo.com>
2025-03-22 21:54:47 +0530

    3rd Commit on 22/03/2025

--- Py_Basics/Py_Basics_001.py
+++ Py_Basics/Py_Basics_001.py
@@ -1,4 +1,12 @@
 
-#Basic Hello Worl Print
+# Basic Hello World Print
 
 print('Hello Worlds')
+
+
+
+# Print Blank Lines
+
+print('Hello World, this is the first line')
+print(8 * '\n')
+print('Hello this is the second line')
