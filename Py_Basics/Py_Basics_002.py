87e359383099b43b5210c6fc1eba520c67600009
mail.subhajit.dey <mail.subhajit.dey@gmail.com>
2025-03-23 07:49:34 +0000

    5th Commit from IDX on 23/03/2025

--- Py_Basics/Py_Basics_002.py
+++ Py_Basics/Py_Basics_002.py
@@ -1,1 +1,23 @@
 
+# Python Input Output
+
+name = input("Enter your name :")
+print('Your name is : '+name)
+
+
+# Print conditional Input from User
+
+age_input = input("Enter your Age : ")
+age = int(age_input)
+
+if age<18:
+   print('You are not adult')
+
+elif age>=18 and age<=29:
+    print('You are adult')
+
+elif age>=30 and age<=45:
+    print('You are youngster')
+
+else:
+    print('You are senior citizen')
