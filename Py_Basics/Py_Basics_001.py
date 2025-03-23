ba1d570dd98bcdb0b4425ae06b0ee952a3eb0f31
mail.subhajit.dey <mail.subhajit.dey@gmail.com>
2025-03-23 04:05:15 +0000

    Commit from Google IDX Online Code Editor on 23/03/2025

--- Py_Basics/Py_Basics_001.py
+++ Py_Basics/Py_Basics_001.py
@@ -1,12 +1,19 @@
-
 # Basic Hello World Print
-
 print('Hello Worlds')
 
 
 
 # Print Blank Lines
-
 print('Hello World, this is the first line')
 print(8 * '\n')
 print('Hello this is the second line')
+
+
+
+# Print the Variables
+a = 10
+b = 25
+c = 'Hello World'
+d = 'Lenovo Legion'
+print(a)
+print(a+b)
